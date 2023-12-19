ea6a49204f53fb773e9fc67cd5643829da1f33da
khLiu <68352452+john-1109@users.noreply.github.com>
2023-12-19 21:47:13 +0800

    Update fir.h

--- testbench/wlos/fir.h
+++ testbench/wlos/fir.h
@@ -7,9 +7,6 @@
 int taps[N] = {0,-10,-9,23,56,63,56,23,-9,-10,0};
 int inputbuffer[N];
 int inputsignal[N] = {1,2,3,4,5,6,7,8,9,10,11};
-#ifdef wlos_act
 int outputsignal[SEQ_LEN];
-#else
-int outputsignal[N];
-#endif
+
 #endif
